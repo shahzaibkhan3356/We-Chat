236a30183a495992b067c48a015a323554507453
SHAHZAIB KHAN <158742203+shahzaibkhan3356@users.noreply.github.com>
2025-07-13 06:05:25 -0700

    Fixes

---
+++ lib/Presentation/Ui/Stories/StoriesPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Storiespage extends StatefulWidget {
+  const Storiespage({super.key});
+
+  @override
+  State<Storiespage> createState() => _StoriespageState();
+}
+
+class _StoriespageState extends State<Storiespage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
